5bf559929639cbcf2df1453a9d4b0ca19ac15d11
noname000002 <73682089+noname000002@users.noreply.github.com>
2021-04-05 10:13:26 +0530

    main.py
    mainfile

---
+++ PyExecutor/main.py
@@ -0,0 +1,36 @@
+import tkinter
+import os
+import sys
+
+    
+rootgui = tkinter.Tk()
+rootgui.title('PyExecutor')
+list = tkinter.Listbox(rootgui, selectmode='SINGLE')
+
+def selected():
+    ActivVal = str(list.get(list.curselection()))
+    for i in filepaths:
+        if i.find(ActivVal) != -1:
+            with open(i,"rb") as file:
+                print("-----------")
+                exe = compile(file.read(),str(i),"exec")
+                exec(exe)
+            
+            
+          
+
+for entry in os.scandir(r'D:\(User)\PyExecutor\Programs'):
+    if entry.path.endswith('.py'):
+        value = entry.path.replace(r'D:\(User)\PyExecutor\Programs', '')
+        list.insert(0,value)
+        filepaths.insert(0,entry.path)
+        
+
+
+
+list.pack()
+executebutton = tkinter.Button(rootgui,text='Execute Program',command=selected)
+executebutton.pack()
+
+rootgui.geometry("300x200")
+rootgui.mainloop() 
